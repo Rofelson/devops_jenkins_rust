7203b18c1f1a93bbeac34777d6b1f6a7ba3d816b
Rofelson <gkouessi@gmail.com>
2023-05-06 02:56:56 +0000

    hard coded guess for pipeline test purposes

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let guess = " 0 ".to_string();
     let guess: u32 = guess.trim().parse().expect("Please type a number");
 
     println!("You guessed: {guess}");
